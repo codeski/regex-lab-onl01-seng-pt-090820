fd654e53895af36e08e0ef7fa89bb742bcbd055d
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-20 20:05:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,13 +2,12 @@ def starts_with_a_vowel?(word)
   word.match(/^[AaEeIiOoUu]/) ? true : false
 end
 
-require 'pry'
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-  text.scan(\w{5})
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
