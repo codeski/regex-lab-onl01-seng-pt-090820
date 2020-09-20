e603ce70288c17c199da589d3e9cc6aa0257c636
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-20 19:30:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,5 @@
 def starts_with_a_vowel?(word)
   word.match(/^[AaEeIiOoUu]/)
-        match.each do |word|
-          expect(starts_with_a_vowel?(word)).to be(true)
-        end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
